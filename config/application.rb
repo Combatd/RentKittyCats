c182f1a4e24a09fd092b5a8a374df6acd2d5f5d4
Combatd <combatdstar@gmail.com>
2020-04-09 03:39:48 -0700

    Set CSRF protection on config/application.rb to false

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module RentKittyCats
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # We won't worry about CSRF attacks today (you're not supposed to know what that is yet!).
+    config.action_controller.default_protect_from_forgery = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
