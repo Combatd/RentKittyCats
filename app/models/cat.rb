2c6ba58719e80e647be8bf417d54bee473d1a208
Combatd <combatdstar@gmail.com>
2020-04-11 09:37:25 -0700

    Define Cat##age in app/models/cat.rb

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,15 @@
 class Cat < ApplicationRecord
+include ActionView::Helpers::DateHelper
+
     validates :birth_date, :name, presence: true
     validates :color, presence: true
     validates :sex, presence: true
+
+    def age
+        years = Date.today.year - self.birth_date.year
+        months = Date.today.month - self.birth_date.month
+        days = Date.today.day - self.birth_date.day
+
+        time_ago = Time.now - years - months - days
+    end
 end
