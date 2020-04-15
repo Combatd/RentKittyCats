79587a99e3e34232f87a7fc7660103516cc4485e
Combatd <combatdstar@gmail.com>
2020-04-15 09:00:39 -0700

    Define CatRentalRequestsController##new, ##create

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,3 +1,22 @@
 class CatRentalRequestsController < ApplicationController
+    def new
+        @cats = Cat.all
+        render :new
+    end
     
+    def create
+        @cat = Cat.find_by(id: cat_rental_request_params[:cat_id])
+        @request = CatRentalRequest.new(cat_rental_request_params)
+
+        if @request.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    def cat_rental_request_params
+        params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date, :status)
+    end
 end
