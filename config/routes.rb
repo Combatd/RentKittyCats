d0b48a1f019be8f2f05d4340fd1482c3d795fd1b
Combatd <combatdstar@gmail.com>
2020-04-26 15:22:12 -0700

    Define UsersController#new, #create, Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :cat_rental_requests, only: [:new, :create, :approve, :deny]
+  resources :users, only: [:new, :create]
 end
