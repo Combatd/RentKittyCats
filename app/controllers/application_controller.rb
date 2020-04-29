36018e0c87a1633276d3b839cdf9331535cc374c
Combatd <combatdstar@gmail.com>
2020-04-28 19:19:47 -0700

    Define ApplicationController#current_user, #logout_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    private
+
+    def current_user
+        if !session[:session_token]
+            return nil
+        else
+            @current_user ||= User.find_by(session_token: session[:session_token])
+        end
+    end
+
+    def logout_user!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
 end
