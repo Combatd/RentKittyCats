92b3d83d25a6a409008bc4d2197ae9fb023128a2
Combatd <combatdstar@gmail.com>
2020-05-03 13:21:54 -0700

    Fix missing @user in UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if user.save
+        if @user.save
             redirect_to cats_url
         else
             flash.new[:errors] = @user.errors.full_messages
