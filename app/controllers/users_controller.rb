4af97eef8189b086f4bf7133a728fda79df0c96c
Combatd <combatdstar@gmail.com>
2020-05-03 13:10:06 -0700

    Redirect to cats_url in UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if user.save
-            redirect_to user_url
+            redirect_to cats_url
         else
             flash.new[:errors] = @user.errors.full_messages
             render :new
